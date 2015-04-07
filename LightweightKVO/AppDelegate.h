819af11efad42c733f8e0dd846d1202038449fd7
nielubowicz <nielubowicz@gmail.com>
2015-04-07 01:19:21 -0400

    Initial Commit

---
+++ LightweightKVO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LightweightKVO
+//
+//  Created by chris nielubowicz on 4/7/15.
+//  Copyright (c) 2015 Assorted Intelligence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LightweightKVO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LightweightKVO
+//
+//  Created by chris nielubowicz on 4/7/15.
+//  Copyright (c) 2015 Assorted Intelligence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
